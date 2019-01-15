fce05bfe70d91fbd26dc11af7d0564724d17b7f0
Priymak1948 <prm48@mail.ru>
2019-01-15 14:39:14 +0300

    Добавили функцию Start

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var http = require("http");
-http.createServer(function(request, response) {
+function start() {
+function onRequest(request, response) {
+console.log("Request received.");
 response.writeHead(200, {"Content-Type": "text/plain"});
 response.write("Hello World");
 response.end();
-}).listen(3000);
+}
+http.createServer(onRequest).listen(3000);
+console.log("Server has started.");
+}
+exports.start = start;
